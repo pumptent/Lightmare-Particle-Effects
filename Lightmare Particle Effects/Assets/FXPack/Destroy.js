d04749afafc3f795f0c6d1de3264417b6397b0c8
Jonathan Schonfeld <jerrycritic@gmail.com>
2017-01-15 13:03:57 -0500

    build01
    Fire effects

---
+++ Lightmare Particle Effects/Assets/FXPack/Destroy.js
@@ -0,0 +1,6 @@
+﻿#pragma strict
+var time : float = 2.0;
+function Start () {
+	Destroy(gameObject, time);
+}
+
---
+++ Lightmare Particle Effects/Assets/FXPack/light.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+private var glow : Light;
+var time : float = 3;
+private var down : boolean = false;
+function Start () {
+	glow = GetComponent.<Light>();
+
+
+}
+
+function Update () {
+	if(glow.intensity >= 0  && down == false){
+		glow.intensity += time * Time.deltaTime;
+	}
+	if(glow.intensity >= 7 && down == false){
+		down = true;
+	}
+
+	if(down){
+		glow.intensity -= time * Time.deltaTime;
+		if(glow.intensity == 0){
+			down = false;
+			Destroy(gameObject);
+		}
+	}
+}
